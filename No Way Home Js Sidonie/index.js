7f53787b33de9e90ada6a72dc9a42a38adcf0acf
Sidoni <99480638+Sidoni@users.noreply.github.com>
2022-05-13 12:38:30 +0300

    Add files via upload

---
+++ No Way Home Js Sidonie/index.js
@@ -0,0 +1,64 @@
+var btn1 = document.getElementById('viewbtn');
+var btn2 = document.getElementById('viewbtn1');
+var btn3 = document.getElementById('viewbtn2');
+var btn4 = document.getElementById('viewbtn3');
+var btn5 = document.getElementById('viewbtn4');
+var btn6 = document.getElementById('viewbtn5');
+var btn7 = document.getElementById('viewbtn6');
+var btn8 = document.getElementById('viewbtn7');
+
+
+function myFunction() {
+    btn1.style.backgroundColor = 'rgba(0,0,0,0)';
+    btn1.style.border = 'solid 2px white';
+    btn1.style.color = '#fff';
+}
+
+function myFunction1() {
+    btn1.style.backgroundColor = '#fff';
+    btn1.style.color = '#b11313';
+}
+
+function myFunction2() {
+    btn2.style.backgroundColor = '#fff';
+    btn2.style.color = '#b11313';
+}
+
+function myFunction3() {
+    btn2.style.backgroundColor = 'rgba(0,0,0,0)';
+    btn2.style.color = '#fff';
+
+}
+
+function myFunction4() {
+    btn3.style.backgroundColor = 'rgba(0,0,0,0)';
+    btn3.style.border = 'solid 2px white';
+    btn3.style.color = '#fff';
+}
+
+function myFunction5() {
+    btn3.style.backgroundColor = '#fff';
+    btn3.style.color = '#b11313';
+}
+
+function myFunction6() {
+    btn4.style.backgroundColor = 'rgba(0,0,0,0)';
+    btn4.style.border = 'solid 2px white';
+    btn4.style.color = '#fff';
+}
+
+function myFunction7() {
+    btn4.style.backgroundColor = '#fff';
+    btn4.style.color = '#b11313';
+}
+
+function myFunction8() {
+    btn5.style.backgroundColor = 'rgba(0,0,0,0)';
+    btn5.style.border = 'solid 2px white';
+    btn5.style.color = '#fff';
+}
+
+function myFunction9() {
+    btn5.style.backgroundColor = '#fff';
+    btn5.style.color = '#b11313';
+}
